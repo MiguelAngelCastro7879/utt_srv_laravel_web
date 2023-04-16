f23febf98969aaa3dd6eb2f5b73d92a2e582bfa5
MiguelAngelCastro <mikeangel7879@gmail.com>
2023-04-16 16:01:41 -0600

    [UPD] AuthSessionController

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,17 +33,17 @@ class AuthenticatedSessionController extends Controller
             if($user->role_id == json_decode($roles->getRoles())->usuario && str_contains($request->url(), env('APP_URL_WEB'))){
                 $request->authenticate();
                 $request->session()->regenerate();
-                return redirect()->intended(RouteServiceProvider::QR_CODE);
+                return redirect()->intended(RouteServiceProvider::VERIFY_CODE);
             } 
             else if($user->role_id == json_decode($roles->getRoles())->administrador && str_contains($request->url(), env('APP_URL_VPN'))){
                 $request->authenticate();
                 $request->session()->regenerate();
-                return redirect()->intended(RouteServiceProvider::QR_CODE);
+                return redirect()->intended(RouteServiceProvider::VERIFY_CODE);
             } 
             else if($user->role_id == json_decode($roles->getRoles())->supervisor){
                 $request->authenticate();
                 $request->session()->regenerate();
-                return redirect()->intended(RouteServiceProvider::QR_CODE);
+                return redirect()->intended(RouteServiceProvider::VERIFY_CODE);
             } 
         }
         return redirect('login');
