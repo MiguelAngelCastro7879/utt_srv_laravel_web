29a8ba1bceb9306fa4e2f1f16001866bfca43a65
MiguelAngelCastro <mikeangel7879@gmail.com>
2023-04-16 20:06:13 -0600

    a

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\QrCodeController;
+use App\Http\Controllers\VerificationCodeController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
